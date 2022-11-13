d4b47d53684363ebcec0753b21564ae7dd58661f
Arseny Prosvetov <arsprosvet@mail.ru>
2022-11-13 22:31:15 +0300

    1 task

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,1 +1,11 @@
-const 
+const addTimer = function () {
+	const elementTimer = document.getElementById('timer');
+	elementTimer.textContent--;
+
+	if (elementTimer.textContent === '0') {
+		alert('Вы победили в конкурсе!');
+		clearInterval(intervalId);
+	}
+}
+
+let intervalId = setInterval(addTimer, 1000);
